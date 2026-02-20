0316b4374a6f828b857f1b79cb6dedaaf9abfffa
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-21 03:30:33 +1100

    Spawning Hospital x 2

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -15,8 +15,8 @@ public class SceneManager : MonoBehaviour
     private Vector3 SoldierPrefab3 = new Vector3(3.5f, 0.0f, 0.0f);
     private Vector3 SoldierPrefab4 = new Vector3(8.0f, 4.0f, 0.0f);
     private Vector3 SoldierPrefab5 = new Vector3(7.5f, -1.0f, 0.0f);
-    private Vector3 HospitalSpawn1 = new Vector3(-7.5f, 0.0f, 0.0f);
-    private Vector3 HospitalSpawn2 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 HospitalSpawn1 = new Vector3(-5.5f, 2.8f, 0.0f);
+    private Vector3 HospitalSpawn2 = new Vector3(-5.6f, -2.5f, 0.0f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,6 +29,9 @@ public class SceneManager : MonoBehaviour
         Instantiate(SoldierPrefab, SoldierPrefab3, Quaternion.identity);
         Instantiate(SoldierPrefab, SoldierPrefab4, Quaternion.identity);
         Instantiate(SoldierPrefab, SoldierPrefab5, Quaternion.identity);
+        Instantiate(HospitalPrefab, HospitalSpawn1, Quaternion.identity);
+        Instantiate(HospitalPrefab, HospitalSpawn2, Quaternion.identity);
+
     }
 
     // Update is called once per frame
