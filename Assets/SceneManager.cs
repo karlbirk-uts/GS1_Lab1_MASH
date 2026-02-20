1e56bee2124b9a5af544a43024467f63fae9e3c5
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-21 03:07:38 +1100

    HelicopterPlayer spawn location (-7.5f, 0.0f, 0.0f)

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -7,11 +7,21 @@ public class SceneManager : MonoBehaviour
     public GameObject SoldierPrefab;
     public GameObject HospitalPrefab;
 
+    private Vector3 PlayerSpawn = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 TreeSpawn1 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 TreeSpawn2 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 SoldierPrefab1 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 SoldierPrefab2 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 SoldierPrefab3 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 SoldierPrefab4 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 SoldierPrefab5 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 HospitalSpawn1 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 HospitalSpawn2 = new Vector3(-7.5f, 0.0f, 0.0f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(PlayerPrefab, transform.position, Quaternion.identity);
+        Instantiate(PlayerPrefab, PlayerSpawn, Quaternion.identity);
     }
 
     // Update is called once per frame
