705e5895cf0e77537bf0b1e108bac772569ecf5d
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-21 02:39:18 +1100

    Using updated InputSystem with arrow keys; Speed 5.0f

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private InputAction inputAction;
+    private Vector2 movementVector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector = inputAction.ReadValue<Vector2>();
+
+    }
+
+    void FixedUpdate()
+    {
+        print(movementVector);
+        rb.linearVelocity = movementVector * speed;
+    }
+}
