a0bb4f5c5c3dbd6dd1e9d71a219305f1646f4ec8
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-21 03:14:42 +1100

    Instantiating Tree x 2

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -6,10 +6,10 @@ public class SceneManager : MonoBehaviour
     public GameObject TreePrefab;
     public GameObject SoldierPrefab;
     public GameObject HospitalPrefab;
-
+    
     private Vector3 PlayerSpawn = new Vector3(-7.5f, 0.0f, 0.0f);
-    private Vector3 TreeSpawn1 = new Vector3(-7.5f, 0.0f, 0.0f);
-    private Vector3 TreeSpawn2 = new Vector3(-7.5f, 0.0f, 0.0f);
+    private Vector3 TreeSpawn1 = new Vector3(1.0f,1.9f,0f);
+    private Vector3 TreeSpawn2 = new Vector3(4.5f,-2.0f,0f);
     private Vector3 SoldierPrefab1 = new Vector3(-7.5f, 0.0f, 0.0f);
     private Vector3 SoldierPrefab2 = new Vector3(-7.5f, 0.0f, 0.0f);
     private Vector3 SoldierPrefab3 = new Vector3(-7.5f, 0.0f, 0.0f);
@@ -22,6 +22,9 @@ public class SceneManager : MonoBehaviour
     void Start()
     {
         Instantiate(PlayerPrefab, PlayerSpawn, Quaternion.identity);
+        Instantiate(TreePrefab, TreeSpawn1, Quaternion.identity);
+        Instantiate(TreePrefab, TreeSpawn2, Quaternion.identity);
+
     }
 
     // Update is called once per frame
