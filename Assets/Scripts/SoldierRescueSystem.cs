5f154b91f1f7a74ab77959d1388e1a81836a83cd
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-25 23:15:31 +1100

    Play AudioSource upon Soldier pickup

--- Assets/Scripts/SoldierRescueSystem.cs
+++ Assets/Scripts/SoldierRescueSystem.cs
@@ -1,15 +1,23 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoldierRescueSystem : MonoBehaviour
 {
     private SceneManagerMASH sceneManagerMASH;
-    public AudioClip collectSoldierSFX;
+    // public AudioClip collectSoldierSFXClip;
+    private AudioSource collectSoldierSFX;
+    // audio.PlayOneShot((AudioClip)Resources.Load(“clank1”));
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sceneManagerMASH = GameObject.FindGameObjectWithTag("SceneManagerMASH").GetComponent<SceneManagerMASH>();
+        // collectSoldierSFXClip = GetComponent<AudioClip>();
+        // collectSoldierSFX.clip = collectSoldierSFXClip;
+        // AudioSource audio = gameObject.addComponent();
+        // collectSoldierSFX.AddComponent<AudioSource>();
+        collectSoldierSFX = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,9 +35,9 @@ public class SoldierRescueSystem : MonoBehaviour
                 Destroy(collider2DObject.gameObject);
                 print("Hit Trigger: Soldier");
                 ++sceneManagerMASH.SoldiersInHelicopter;
-                
+                // collectSoldierSFX.PlayOneShot();
+                collectSoldierSFX.Play();
             }
-
         }
         else if (collider2DObject.gameObject.tag == "Hospital")
         {
