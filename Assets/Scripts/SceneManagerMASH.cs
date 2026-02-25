7c730292c7afb19ff248098f29e988043950e389
karlbirk-uts <172787504+karlbirk-uts@users.noreply.github.com>
2026-02-25 13:57:39 +1100

    SceneManagerMASH.endGameDeath() called when HelicopterPlayer hits Tree trigger collider

--- Assets/Scripts/SceneManagerMASH.cs
+++ Assets/Scripts/SceneManagerMASH.cs
@@ -12,13 +12,15 @@ public class SceneManagerMASH : MonoBehaviour
     private Vector3 PlayerSpawn = new Vector3(-7.5f, 0.0f, 0.0f);
     private Vector3 TreeSpawn1 = new Vector3(1.0f, 1.9f, 0.0f);
     private Vector3 TreeSpawn2 = new Vector3(4.5f, -2.0f, 0.0f);
-    private Vector3 SoldierPrefab1 = new Vector3(2.7f, 3.4f, 0.0f);
-    private Vector3 SoldierPrefab2 = new Vector3(6.3f, -3.1f, 0.0f);
-    private Vector3 SoldierPrefab3 = new Vector3(3.5f, 0.0f, 0.0f);
-    private Vector3 SoldierPrefab4 = new Vector3(8.0f, 4.0f, 0.0f);
-    private Vector3 SoldierPrefab5 = new Vector3(7.5f, -1.0f, 0.0f);
+    private Vector3 SoldierSpawn1 = new Vector3(2.7f, 3.4f, 0.0f);
+    private Vector3 SoldierSpawn2 = new Vector3(6.3f, -3.1f, 0.0f);
+    private Vector3 SoldierSpawn3 = new Vector3(3.5f, 0.0f, 0.0f);
+    private Vector3 SoldierSpawn4 = new Vector3(8.0f, 4.0f, 0.0f);
+    private Vector3 SoldierSpawn5 = new Vector3(7.5f, -1.0f, 0.0f);
     private Vector3 HospitalSpawn1 = new Vector3(-5.5f, 2.8f, 0.0f);
     private Vector3 HospitalSpawn2 = new Vector3(-5.6f, -2.5f, 0.0f);
+
+    private Vector3 GameOverSpawn = new Vector3(0.0f, 0.0f, 0.0f);
 
     public int SoldiersInField = 5;
     public int SoldiersInHelicopter = 0;
@@ -30,17 +32,18 @@ public class SceneManagerMASH : MonoBehaviour
         Instantiate(PlayerPrefab, PlayerSpawn, Quaternion.identity);
         Instantiate(TreePrefab, TreeSpawn1, Quaternion.identity);
         Instantiate(TreePrefab, TreeSpawn2, Quaternion.identity);
-        Instantiate(SoldierPrefab, SoldierPrefab1, Quaternion.identity);
-        Instantiate(SoldierPrefab, SoldierPrefab2, Quaternion.identity);
-        Instantiate(SoldierPrefab, SoldierPrefab3, Quaternion.identity);
-        Instantiate(SoldierPrefab, SoldierPrefab4, Quaternion.identity);
-        Instantiate(SoldierPrefab, SoldierPrefab5, Quaternion.identity);
+        Instantiate(SoldierPrefab, SoldierSpawn1, Quaternion.identity);
+        Instantiate(SoldierPrefab, SoldierSpawn2, Quaternion.identity);
+        Instantiate(SoldierPrefab, SoldierSpawn3, Quaternion.identity);
+        Instantiate(SoldierPrefab, SoldierSpawn4, Quaternion.identity);
+        Instantiate(SoldierPrefab, SoldierSpawn5, Quaternion.identity);
         Instantiate(HospitalPrefab, HospitalSpawn1, Quaternion.identity);
         Instantiate(HospitalPrefab, HospitalSpawn2, Quaternion.identity);
 
         SoldiersInField = GameObject.FindGameObjectsWithTag("Soldier").Length;
 
-        Instantiate(Prefab)
+        Instantiate(GameOverPrefab, GameOverSpawn, Quaternion.identity);
+        
     }
 
     // Update is called once per frame
